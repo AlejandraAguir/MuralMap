7fca5e0f818cbbe4a50ca4aa01577641b36643e9
AlejandraAguir <78757057+AlejandraAguir@users.noreply.github.com>
2021-05-05 11:54:03 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1QNlgbz6BEHfa_tfFm9NejB9N2B2tlOdGwJmz8jK7oa4/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1rwJnTBOdjIIZy81rdQTHthDEgio2rZiZI0GS3ocqKfQ/edit?usp=sharing';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
